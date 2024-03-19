9d79420645aa0dfab660a3a077d425bc6454cffc
Vincent van Aalten <vincent.aalten@hotmail.com>
2024-03-19 17:10:59 +0100

    alignment

--- main.py
+++ main.py
@@ -1,30 +1,43 @@
-import whisperx
-import gc 
 import os
+import re
 
-input_dir = "./input/Book 1 mp3/"
-mp3_files = []
-for file in os.listdir(input_dir):
-    if file.endswith(".mp3"):
-        mp3_files.append(input_dir+file)
+import ebooklib
+from ebooklib import epub
 
+import stable_whisper
 
+from bs4 import BeautifulSoup
 
-device = "cuda" 
-audio_file = mp3_files[0]
-batch_size = 6 # reduce if low on GPU mem
-compute_type = "int8" # change to "int8" if low on GPU mem (may reduce accuracy)
+def get_lines(content):
+    soup = BeautifulSoup(content, 'html.parser')
+    lines = soup.get_text().split("\n")
+    lines = [x for x in lines if x]
+    lines = [x.replace('\u3000', "") for x in lines]
+    text = "\n".join(lines)
+    text = text.replace("。","。\n")
+    return text
 
-# 1. Transcribe with original whisper (batched)
-print("loading model...")
-model = whisperx.load_model("large-v2", device, compute_type=compute_type, language="ja")
+def align(text, audio_file):
+    model = stable_whisper.load_model('base')
+    result = model.align(audio_file, text, language='ja', original_split=True)
+    
+    print(result.to_srt_vtt(word_level=False))
+    print(result.to_txt())
 
-# save model to local path (optional)
-# model_dir = "/path/"
-# model = whisperx.load_model("large-v2", device, compute_type=compute_type, download_root=model_dir)
+def read_ebook():
+    book = epub.read_epub('./input/Book 1.epub')
+    for i in book.get_items_of_type(ebooklib.ITEM_DOCUMENT):
+        print(i.get_name())
+        if i.get_name() == "text/part0007.html":
+            return get_lines(i.get_content())
 
-print("transcribing " + audio_file)
-audio = whisperx.load_audio(audio_file)
-result = model.transcribe(audio, batch_size=batch_size)
-print(result["segments"]) # before alignment
-
+if __name__ == "__main__":
+    input_dir = "./input/Book 1 mp3/"
+    mp3_files = []
+    for file in os.listdir(input_dir):
+        if file.endswith(".mp3"):
+            mp3_files.append(input_dir+file)
+    print("\n".join([(str(x)+": "+i) for (x,i) in enumerate(mp3_files)]))
+    
+    text = read_ebook()
+    align(text, mp3_files[3])
